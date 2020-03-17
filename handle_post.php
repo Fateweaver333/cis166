5202fcebac703eda939ca31d45e742d2ea2b3bcf
Fateweaver333 <60595644+Fateweaver333@users.noreply.github.com>
2020-03-16 22:24:53 -0700

    Add files via upload

--- handle_post.php
+++ handle_post.php
@@ -15,14 +15,20 @@
 	// Get the values from the $_POST array:
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
-$posting = $_POST['posting'];
+$posting = nl2br($_POST['posting'],false);
 
 	// Create a full name variable:
 $name = $first_name . ' ' . $last_name;
 
+	// Adjust for HTML tags:
+$html_post = htmlentities($_POST['posting']);
+$strip_post = strip_tags($_POST['posting']);
+
 	// Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>$posting</p></div>";
+<p>Original: $posting</p>
+<p>Entity: $html_post</p>
+<p>Stripped: $strip_post</p></div>";
 
 ?>
 </body>
