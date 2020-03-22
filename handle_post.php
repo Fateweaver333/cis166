851beb2cbf732f72ada5e5dae5c3abcef2f1a761
Fateweaver333 <60595644+Fateweaver333@users.noreply.github.com>
2020-03-22 00:22:08 -0700

    Add files via upload

--- handle_post.php
+++ handle_post.php
@@ -20,15 +20,15 @@ $posting = nl2br($_POST['posting'],false);
 	// Create a full name variable:
 $name = $first_name . ' ' . $last_name;
 
-	// Adjust for HTML tags:
-$html_post = htmlentities($_POST['posting']);
-$strip_post = strip_tags($_POST['posting']);
-
 	// Print a message:
 print "<div>Thank you, $name, for your posting:
-<p>Original: $posting</p>
-<p>Entity: $html_post</p>
-<p>Stripped: $strip_post</p></div>";
+<p>$posting</p></div>";
+
+	// Make a link to another page:
+$name = urldecode($name);
+$email = urldecode($_POST['email']);
+print "<p>Click <a href=\"thanks.php?
+name=$name&email=$email\">here</a> to continue.</p>";
 
 ?>
 </body>
