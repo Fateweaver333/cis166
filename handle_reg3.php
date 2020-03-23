034de1ed42ed2e2eaca1b299fb549ce644d18717
Fateweaver333 <60595644+Fateweaver333@users.noreply.github.com>
2020-03-22 22:20:16 -0700

    Add files via upload

---
+++ handle_reg3.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Registration</title>
+	<style type="text/css" media="screen">
+	.error { color: red; } </style>
+</head>
+<body>
+<h1>Registration Results</h1>
+<?php 
+	// Script 6.3 - handle_reg.php #2
+	/* This script receives seven values from register.html:
+	email, password, confirm, year, terms, color, submit */
+
+	// Flag variable to track success:
+$okay = true;
+	
+	// Validate email address:
+if (empty($_POST['email']))
+{ print '<p class="error">Please enter your email address.</p>';
+	$okay = false;
+}
+	// Validate the password:
+if (empty($_POST['password']))
+{ print '<p class="error">Please enter your password.</p>';
+	$okay = false;
+}
+	// Validate the birth year:
+if (is_numeric($_POST['year']))
+{ 	$age = 2016 - $_POST['year'];	// Calculate age year.
+} else {
+	print '<p class="error">Please enter year you were born in as four digits.</p>';
+	$okay = false;
+	
+	// If there are no errors, print a success message:
+if ($okay)	
+{ 
+	print '<p>You have been successfully registered (but not really).</p>';
+	print "<p>You will turn $age this year.</p>";
+}
+?>
+
+</body>
+</html>
