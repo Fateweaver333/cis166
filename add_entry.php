d2f718006d927e1ecc96de1dfa56ae3549fb3289
Fateweaver333 <60595644+Fateweaver333@users.noreply.github.com>
2020-05-03 06:25:30 -0700

    Add files via upload

--- add_entry.php
+++ add_entry.php
@@ -7,39 +7,42 @@
 <body>
 <h1>Add a Blog Entry</h1>
 <?php 
-	// Script 12.4 - add_entry.php
-	/* This script adds a blog entry to the database. */
+	// Script 12.5 - add_entry.php #2
+	/* This script adds a blog entry to the database. It now does so securely! */
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') { // Handle the form.
 
-	// Validate the form data:
-$problem = FALSE;
+	// Connect and select:
+	$dbc = mysqli_connect('localhost', 'username', 'password', 'myblog');
+
+	//Set the character set:
+	mysqli_set_charset($dbc, 'utf8');
+
+	// Validate and secure the form data:
+	$problem = FALSE;
 	if (!empty($_POST['title']) && !empty($_POST['entry'])) {
-		$title = trim(strip_tags($_POST['title']));
-		$entry = trim(strip_tags($_POST['entry']));
+		$title = mysqli_real_escape_string($dbc, trim(strip_tags($_POST['title'])));
+		$entry = mysqli_real_escape_string($dbc, trim(strip_tags($_POST['entry'])));
 	} else {
 		print '<p style="color: red;">Please submit both a title and an entry.</p>';
 		$problem = TRUE;
 	}
 
-if (!$problem) {
+	if (!$problem) {
 
-	// Connect and select:
-		$dbc = mysqli_connect('localhost', 'username', 'password', 'myblog');
-
-	// Define the query:
+		// Define the query:
 		$query = "INSERT INTO entries (id, title, entry, date_entered) VALUES (0, '$title', '$entry', NOW())";
 
-	// Execute the query:
+		// Execute the query:
 		if (@mysqli_query($dbc, $query)) {
 			print '<p>The blog entry has been added!</p>';
 		} else {
 			print '<p style="color: red;">Could not add the entry because:<br>' . mysqli_error($dbc) . '.</p><p>The query being run was: ' . $query . '</p>';
 		}
 
-		mysqli_close($dbc); // Close the connection.
-
 	} // No problem!
+
+	mysqli_close($dbc); // Close the connection.
 
 } // End of form submission IF.
 
